14446f2d79a1d9113f55657b67004ded6261cf46
SantiagoTrifonArevalo <santiagodtrifon@gmail.com>
2023-07-25 20:24:39 +0200

    make

---
+++ main.js
@@ -0,0 +1,89 @@
+// ejercicio-1
+function changeBackgroundColor() {
+    // Generar valores aleatorios para RGB (rojo, verde, azul)
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    // Crear el string de color en formato RGB
+    const randomColor = `rgb(${red}, ${green}, ${blue})`;
+
+    // Cambiar el color de fondo de toda la página
+    document.body.style.backgroundColor = randomColor;
+}
+
+//  Ejercicio-2
+// He aquí un contador y dos botones. Cuando se haga click en el botón de aumentar, el contador debe aumentar en 1. Cuando se haga click en el botón de disminuir, debe disminuir en 1.
+// 0
+// +1 -1
+// Obtenemos el contador y los botones por su id
+const counterElement = document.getElementById('counter');
+const increaseButton = document.getElementById('increase');
+const decreaseButton = document.getElementById('decrease');
+
+// Inicializamos el contador
+let counterValue = 0;
+
+// Función para actualizar el valor del contador en el elemento del DOM
+function updateCounter() {
+  counterElement.textContent = counterValue;
+}
+
+// Función para aumentar el contador en 1
+function increaseCounter() {
+  counterValue++;
+  updateCounter();
+}
+
+// Función para disminuir el contador en 1
+function decreaseCounter() {
+  counterValue--;
+  updateCounter();
+}
+
+// Asignamos los eventos a los botones
+increaseButton.addEventListener('click', increaseCounter);
+decreaseButton.addEventListener('click', decreaseCounter);
+
+// Actualizamos el valor inicial del contador
+updateCounter();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Este array debe usarse para el ejercicio 10
+const data = [
+    { id: 1, name: 'bulbasaur', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png' },
+    { id: 2, name: 'ivysaur', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png' },
+    { id: 3, name: 'venusaur', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png' },
+    { id: 4, name: 'charmander', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png' },
+    { id: 5, name: 'charmeleon', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png' },
+    { id: 6, name: 'charizard', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png' },
+    { id: 7, name: 'squirtle', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png' },
+    { id: 8, name: 'wartortle', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png' },
+    { id: 9, name: 'blastoise', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png' },
+    { id: 10, name: 'caterpie', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png' },
+    { id: 11, name: 'metapod', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png' },
+    { id: 12, name: 'butterfree', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png' },
+    { id: 13, name: 'weedle', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png' },
+    { id: 14, name: 'kakuna', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/14.png' },
+    { id: 15, name: 'beedrill', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png' },
+    { id: 16, name: 'pidgey', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png' },
+    { id: 17, name: 'pidgeotto', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png' },
+    { id: 18, name: 'pidgeot', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/18.png' },
+    { id: 19, name: 'rattata', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png' },
+    { id: 20, name: 'raticate', img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/20.png' }
+]
